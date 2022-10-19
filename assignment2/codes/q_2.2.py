5b7bf5f899a0f62bcfd2dd142d940539288eb0ce
Devananth V <113325565+ep20btech11004@users.noreply.github.com>
2022-10-19 19:36:02 +0530

    Add files via upload

---
+++ assignment2/codes/q_2.2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+X = [1, 1]
+
+for i in range(20):
+    n = X[i] + X[i+1]
+    X.append(n)
+
+#plt.plot(X)
+plt.stem(range(len(X)), X)
+plt.grid()
+plt.xlabel('$n$')
+plt.ylabel('$x(n)$')
+plt.tight_layout()
+plt.savefig('../figs/q_2-2.png')
+plt.show()
